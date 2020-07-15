348cffc4a27ab814a8f4a4c2533d5ff3d6825533
SmileyHu <53206761+SmileyHu@users.noreply.github.com>
2020-07-15 16:34:03 +0800

    Create ex.java

---
+++ ex.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class sublist {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+        result.add(new ArrayList<>());
+
+
+
+
+        return new ArrayList<>();
+    }
+}
