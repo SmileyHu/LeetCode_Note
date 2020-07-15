d3d4863e29b7051cdd88d8022a033bae8f33766f
Smiley <19946251526@163.com>
2020-07-15 17:10:50 +0800

    hll change

--- ex.java
+++ ex.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class sublist {
+class sublist {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result=new ArrayList<>();
         result.add(new ArrayList<>());
+        ArrayList<Integer> arr=new ArrayList<>(3);
+        result.add(arr);
 
 
-
-
-        return new ArrayList<>();
+        return result;
     }
 }
