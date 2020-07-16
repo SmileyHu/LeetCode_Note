f0079d1ee8558cbfdacf2b464fcf0f6a49a46f10
Smiley <19946251526@163.com>
2020-07-16 10:01:40 +0800

    d

--- ex.java
+++ ex.java
@@ -2,13 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 class sublist {
+    public static void main(String[] args) {
+
+    }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result=new ArrayList<>();
         result.add(new ArrayList<>());
-        ArrayList<Integer> arr=new ArrayList<>(3);
-        result.add(arr);
-
-
+        for(int n:nums) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int len = result.size();
+            for (int i = 0; i < len; i++) {
+                list.addAll(result.get(i));
+                list.add(n);
+                result.add(list);
+            }
+        }
         return result;
     }
 }
